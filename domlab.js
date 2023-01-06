d884ed9f04281a74b46f41ec4559ece005b3ed45
Melva <78463026+melsabiti@users.noreply.github.com>
2023-01-06 11:33:20 -0500

    Add files via upload

---
+++ domlab.js
@@ -0,0 +1,15 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
+
+
+let mainL = document.querySelector(".mainE1")
+mainL.classList.add("flex-ctr")
+
+let menu = document.querySelector("top-menuE1")
+menu.classList.add("flex-around")
